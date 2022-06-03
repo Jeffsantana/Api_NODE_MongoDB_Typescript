b807b838dc6b4458c336403823ed257a6491822d
Jeffsantana <jeffersonsantana2008@hotmail.com>
2022-06-03 10:04:01 -0300

    add cors

--- app.ts
+++ app.ts
@@ -1,11 +1,17 @@
 import express, { NextFunction, Request, Response } from 'express'
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import routes from './src/routes'
 import { AppError } from './shared/AppError';
 import database from './src/config/mongoose'
 
 const app = express();
-
-app.use(express.json())
+// ================ External MIDDLEWARES ================ //
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json({ limit: '25mb' }));
 
 app.use(routes)
 database.mongoConnect()
